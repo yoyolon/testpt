d7b2e67271cd7d350be68a8ad62c335d78c91b28
yoyolon <yoyograph74@gmail.com>
2022-12-06 04:03:54 +0900

    [fix] 交差判定の簡素化

--- scr/Sphere.cpp
+++ scr/Sphere.cpp
@@ -2,15 +2,17 @@
 
 // レイと球の交差判定
 bool Sphere::intersect(const Ray& r, float t_min, float t_max, intersection& p) const {
-	// 二次方程式の判別式D = b^2 - 4acを利用
-	auto temp = r.get_orig() - center;
+	// 二次方程式の判別式D = (b/2)^2 - acを利用(bは偶数)
+	// TODO: 数式の簡単化
+	auto temp = r.get_origin() - center;
 	auto a = r.get_dir().length2();
-	auto b = 2 * dot(r.get_dir(), temp);
+	auto b_half = dot(r.get_dir(), temp);
 	auto c = temp.length2() - radius * radius;
-	auto D = b * b - 4 * a * c;
+	auto D = b_half * b_half - a * c;
 
 	if (D < 0) return false;
-	auto d = std::sqrt(D);
+	auto b = b_half * 2;
+	auto d = 2 * std::sqrt(D);
 	auto t = (-b - d) / (2 * a);
 	if (t < t_min || t > t_max) {
 		t = (-b + d) / (2 * a);

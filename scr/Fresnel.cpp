d1c3d4440d4b086754db42d6d1007b5777504d83
yoyolon <yoyograph74@gmail.com>
2022-12-15 00:58:40 +0900

    [fix] コーディング規則

--- scr/Fresnel.cpp
+++ scr/Fresnel.cpp
@@ -1,59 +1,118 @@
 #include "Fresnel.h"
 #include <complex>
 
-// *** UtilityŠÖ” ***
-// ƒtƒŒƒlƒ‹‚Ì®
+/**
+* @brief s•ÎŒõ‚ÌƒtƒŒƒlƒ‹”½ËŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] ni   :”}¿i‚Ì‹üÜ—¦
+* @param[in] nj   :”}¿j‚Ì‹üÜ—¦
+* @param[in] cosi :”}¿i‚Ö‚Ì“üËŠp—]Œ·
+* @param[in] cosj :”}¿j‚Ö‚Ì“üËŠp—]Œ·
+* @return float   :s•ÎŒõ‚ÌƒtƒŒƒlƒ‹”½ËŒW”
+*/
 inline float fresnel_rs(float ni, float nj, float cosi, float cosj) {
-	return (ni * cosi - nj * cosj) / (ni * cosi + nj * cosj);
-}
-inline float fresnel_ts(float ni, float nj, float cosi, float cosj) {
-	return (ni * cosi + ni * cosi) / (ni * cosi + nj * cosj);
-}
-inline float fresnel_rp(float ni, float nj, float cosi, float cosj) {
-	return (nj * cosi - ni * cosj) / (ni * cosj + nj * cosi);
-}
-inline float fresnel_tp(float ni, float nj, float cosi, float cosj) {
-	return (ni * cosi + ni * cosi) / (ni * cosj + nj * cosi);
-}
-// ”½ËŒW”
-static std::complex<float> composit_r(float r, float R, float phi) {
-	std::complex<float>i(0.f, 1.f);
-	return (r + R * exp(i * 2.0f * phi)) / (1.0f + r * R * exp(i * 2.0f * phi));
-}
-// “§‰ßŒW”
-static std::complex<float> composit_t(float r, float R, float t, float T, float phi) {
-	std::complex<float>i(0.f, 1.f);
-	return (t * T * exp(i * phi)) / (1.0f + r * R * exp(i * 2.0f * phi));
+    return (ni * cosi - nj * cosj) / (ni * cosi + nj * cosj);
 }
 
-// ”––ŒŠ±Â”½Ë—¦
+/**
+* @brief s•ÎŒõ‚ÌƒtƒŒƒlƒ‹“§‰ßŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] ni   :”}¿i‚Ì‹üÜ—¦
+* @param[in] nj   :”}¿j‚Ì‹üÜ—¦
+* @param[in] cosi :”}¿i‚Ö‚Ì“üËŠp—]Œ·
+* @param[in] cosj :”}¿j‚Ö‚Ì“üËŠp—]Œ·
+* @return float   :s•ÎŒõ‚ÌƒtƒŒƒlƒ‹“§‰ßŒW”
+*/
+inline float fresnel_ts(float ni, float nj, float cosi, float cosj) {
+    return (ni * cosi + ni * cosi) / (ni * cosi + nj * cosj);
+}
+
+/**
+* @brief p•ÎŒõ‚ÌƒtƒŒƒlƒ‹”½ËŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] ni   :”}¿i‚Ì‹üÜ—¦
+* @param[in] nj   :”}¿j‚Ì‹üÜ—¦
+* @param[in] cosi :”}¿i‚Ö‚Ì“üËŠp—]Œ·
+* @param[in] cosj :”}¿j‚Ö‚Ì“üËŠp—]Œ·
+* @return float   :s•ÎŒõ‚ÌƒtƒŒƒlƒ‹”½ËŒW”
+*/
+inline float fresnel_rp(float ni, float nj, float cosi, float cosj) {
+    return (nj * cosi - ni * cosj) / (ni * cosj + nj * cosi);
+}
+
+/**
+* @brief p•ÎŒõ‚ÌƒtƒŒƒlƒ‹“§‰ßŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] ni   :”}¿i‚Ì‹üÜ—¦
+* @param[in] nj   :”}¿j‚Ì‹üÜ—¦
+* @param[in] cosi :”}¿i‚Ö‚Ì“üËŠp—]Œ·
+* @param[in] cosj :”}¿j‚Ö‚Ì“üËŠp—]Œ·
+* @return float   :s•ÎŒõ‚ÌƒtƒŒƒlƒ‹“§‰ßŒW”
+*/
+inline float fresnel_tp(float ni, float nj, float cosi, float cosj) {
+    return (ni * cosi + ni * cosi) / (ni * cosj + nj * cosi);
+}
+
+
+/**
+* @brief Š±Â‚ğl—¶‚µ‚½ƒtƒŒƒlƒ‹”½ËŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] r            :ƒtƒŒƒlƒ‹”½ËŒW”
+* @param[in] R            :ƒtƒŒƒlƒ‹”½ËŒW”
+* @param[in] phi          :ˆÊ‘Š·
+* @return complex<float>  :‡¬”½ËŒW”
+* @note: Ql: Hirayama+. "Visualization of optical phenomena caused by multilayer films based on wave optics". 2001.
+*/
+static std::complex<float> composit_r(float r, float R, float phi) {
+    std::complex<float>i(0.f, 1.f);
+    return (r + R * exp(i * 2.0f * phi)) / (1.0f + r * R * exp(i * 2.0f * phi));
+}
+/**
+* @brief Š±Â‚ğl—¶‚µ‚½ƒtƒŒƒlƒ‹“§‰ßŒW”‚ğŒvZ‚·‚éŠÖ”
+* @param[in] r            :ƒtƒŒƒlƒ‹”½ËŒW”
+* @param[in] R            :ƒtƒŒƒlƒ‹”½ËŒW”
+* @param[in] t            :ƒtƒŒƒlƒ‹“§‰ßŒW”
+* @param[in] T            :ƒtƒŒƒlƒ‹“§‰ßŒW”
+* @param[in] phi          :ˆÊ‘Š·
+* @return complex<float>  :‡¬“§‰ßŒW”
+* @note: Ql: Hirayama+. "Visualization of optical phenomena caused by multilayer films based on wave optics". 2001.
+*/
+static std::complex<float> composit_t(float r, float R, float t, float T, float phi) {
+    std::complex<float>i(0.f, 1.f);
+    return (t * T * exp(i * phi)) / (1.0f + r * R * exp(i * 2.0f * phi));
+}
+
+/**
+* @brief Š±Â‚ğl—¶‚µ‚½”½Ë—¦‚ğŒvZ‚·‚éŠÖ”
+* @param[in] cos0 :“üËŠp—]Œ·
+* @param[in] d    :”––Œ‚Ì–ŒŒú
+* @param[in] n0   :“üË”}¿‚Ì‹üÜ—¦
+* @param[in] n1   :”––Œ‚Ì‹üÜ—¦
+* @param[in] n2   :oË”}¿‚Ì‹üÜ—¦
+* @return Vec3    :”––ŒŠ±Â‚ğl—¶‚µ‚½”½Ë—¦
+* @note: Ql: Hirayama+. "Visualization of optical phenomena caused by multilayer films based on wave optics". 2001.
+*/
 Vec3 iridterm(float cos0, float d, float n0, float n1, float n2) {
-	// “§‰ßŠpŒvZ
-	float sin0 = std::sqrt(std::max(0.0f, 1.0f - cos0 * cos0));
-	float sin1 = n0 / n1 * sin0;
-	if (sin1 >= 1.0) return  Vec3(1.0f, 1.0f, 1.0f); // ‘S”½Ë
-	float cos1 = std::sqrt(std::max(0.0f, 1.0f - sin1 * sin1));
-	float sin2 = n0 / n2 * sin0;
-	if (sin2 >= 1.0) return Vec3(1.0f, 1.0f, 1.0f);  // ‘S”½Ë
-	float cos2 = std::sqrt(std::max(0.0f, 1 - sin2 * sin2));
-	// ƒtƒŒƒlƒ‹”½ËŒW”ŒvZ
-	float r01s = fresnel_rs(n0, n1, cos0, cos1);
-	float r12s = fresnel_rs(n1, n2, cos1, cos2);
-	float r01p = fresnel_rp(n0, n1, cos0, cos1);
-	float r12p = fresnel_rp(n1, n2, cos1, cos2);
-	// ‡¬”½Ë—¦ŒvZ
-	float lambda[3] = { 640.0, 540.0, 450.0 };
-	float rgb[3] = { 0.0 };
-	std::complex<float>r_s[3];
-	std::complex<float>r_p[3];
-	for (int i = 0; i < 3; i++) {
-		float phi = 2 * pi / lambda[i] * n1 * d * cos1;
-		//std::complex<float> phi_c(phi, 0.f);
-		r_s[i] = composit_r(r01s, r12s, phi); // ‡¬”½ËŒW”(s”g)
-		r_p[i] = composit_r(r01p, r12p, phi); // ‡¬”½ËŒW”(p”g)
-		rgb[i] = (norm(r_s[i]) + norm(r_p[i])) / 2;
-	}
-	return Vec3(rgb);
+    // “§‰ßŠpŒvZ
+    float sin0 = std::sqrt(std::max(0.0f, 1.0f - cos0 * cos0));
+    float sin1 = n0 / n1 * sin0;
+    if (sin1 >= 1.0) return  Vec3(1.0f, 1.0f, 1.0f); // ‘S”½Ë
+    float cos1 = std::sqrt(std::max(0.0f, 1.0f - sin1 * sin1));
+    float sin2 = n0 / n2 * sin0;
+    if (sin2 >= 1.0) return Vec3(1.0f, 1.0f, 1.0f);  // ‘S”½Ë
+    float cos2 = std::sqrt(std::max(0.0f, 1 - sin2 * sin2));
+    // ƒtƒŒƒlƒ‹”½ËŒW”ŒvZ
+    float r01s = fresnel_rs(n0, n1, cos0, cos1);
+    float r12s = fresnel_rs(n1, n2, cos1, cos2);
+    float r01p = fresnel_rp(n0, n1, cos0, cos1);
+    float r12p = fresnel_rp(n1, n2, cos1, cos2);
+    // ‡¬”½Ë—¦ŒvZ
+    float lambda[3] = { 640.0, 540.0, 450.0 };
+    float rgb[3] = { 0.0 };
+    std::complex<float>r_s[3];
+    std::complex<float>r_p[3];
+    for (int i = 0; i < 3; i++) {
+        float phi = 2 * pi / lambda[i] * n1 * d * cos1;
+        r_s[i] = composit_r(r01s, r12s, phi); // ‡¬”½ËŒW”(s”g)
+        r_p[i] = composit_r(r01p, r12p, phi); // ‡¬”½ËŒW”(p”g)
+        rgb[i] = (norm(r_s[i]) + norm(r_p[i])) / 2;
+    }
+    return Vec3(rgb);
 }
 
 
@@ -61,35 +120,35 @@ Vec3 iridterm(float cos0, float d, float n0, float n1, float n2) {
 Fresnel::~Fresnel() {}
 
 
-// *** Schlick‹ß— ***
+// *** SchlickƒtƒŒƒlƒ‹ ***
 FresnelSchlick::FresnelSchlick(Vec3 _F0)
-	: F0(_F0) {}
+    : F0(_F0) {}
 
 Vec3 FresnelSchlick::evaluate(float cos_theta) const {
-	return F0 + (Vec3(1.0f,1.0f,1.0f) - F0) * (float)std::pow(1.0f - cos_theta, 5);
+    return F0 + (Vec3(1.0f,1.0f,1.0f) - F0) * (float)std::pow(1.0f - cos_theta, 5);
 }
 
 
-// *** —U“d‘Ì ***
+// *** —U“d‘ÌƒtƒŒƒlƒ‹ ***
 FresnelDielectric::FresnelDielectric(float _ni, float _no) 
-	: ni(_ni), no(_no) {}
+    : ni(_ni), no(_no) {}
 
 Vec3 FresnelDielectric::evaluate(float cos_theta) const {
-	float sin_theta = std::sqrt(std::max(0.0f, 1.0f - cos_theta * cos_theta));
-	float sinO = no / ni * sin_theta;
-	if (sinO >= 1.0f) return Vec3(0.0f, 0.0f, 0.0f);
-	float cosO = std::sqrt(std::max(0.0f, 1.0f - sinO * sinO));
-	float Rs = fresnel_rs(ni, no, cos_theta, cosO);
-	float Rp = fresnel_rp(ni, no, cos_theta, cosO);
-	float R =  (Rs * Rs + Rp * Rp) / 2;
-	return Vec3(R, R, R);
+    float sin_theta = std::sqrt(std::max(0.0f, 1.0f - cos_theta * cos_theta));
+    float sinO = no / ni * sin_theta;
+    if (sinO >= 1.0f) return Vec3(0.0f, 0.0f, 0.0f);
+    float cosO = std::sqrt(std::max(0.0f, 1.0f - sinO * sinO));
+    float Rs = fresnel_rs(ni, no, cos_theta, cosO);
+    float Rp = fresnel_rp(ni, no, cos_theta, cosO);
+    float R =  (Rs * Rs + Rp * Rp) / 2;
+    return Vec3(R, R, R);
 }
 
 
-// *** ”––ŒŠ±Â ***
+// *** ”––ŒŠ±ÂƒtƒŒƒlƒ‹ ***
 FresnelThinfilm::FresnelThinfilm(float _d, float _ni, float _nf, float _no)
-	: d(_d), ni(_ni), nf(_nf), no(_no) {}
+    : d(_d), ni(_ni), nf(_nf), no(_no) {}
 
 Vec3 FresnelThinfilm::evaluate(float cos_theta) const {
-	return iridterm(cos_theta, d, ni, nf, no);
+    return iridterm(cos_theta, d, ni, nf, no);
 }
--- scr/Fresnel.h
+++ scr/Fresnel.h
@@ -1,48 +1,75 @@
+/**
+* @file  Fresnel.h
+* @brief フレネル式
+*/
+
 #pragma once
 
 #include "Vec3.h"
 #include <complex>
 
-// *** フレネルクラス ***
+/** フレネル式の抽象クラス */
 class Fresnel {
 public:
-	virtual ~Fresnel();
-	virtual Vec3 evaluate(float cos_theta) const = 0;
+    virtual ~Fresnel();
+
+    /**
+    * @brief 入射角によるフレネル反射率を計算する関数
+    * @param[in] cos_theta :入射角余弦
+    * @return Vec          :フレネル反射率
+    * @note: 入射角は法線ベクトルと入射方向ベクトルがなす角
+    */
+    virtual Vec3 evaluate(float cos_theta) const = 0;
 };
 
-// *** Schlick近似 ***
+
+/** Schlickフレネルクラス */
 class FresnelSchlick : public Fresnel {
 public:
-	FresnelSchlick(Vec3 _F0);
-	Vec3 evaluate(float cos_theta) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in]  _F0 :垂直入射でのフレネル反射率
+    */
+    FresnelSchlick(Vec3 _F0);
+
+    Vec3 evaluate(float cos_theta) const override;
 
 private:
-	Vec3 F0;
+    Vec3 F0; /**< 垂直入射でのフレネル反射率 */
 };
 
-// *** 誘電体 ***
+
+/** 誘電体フレネルクラス */
 class FresnelDielectric : public Fresnel {
 public:
-	FresnelDielectric(float _ni, float _no);
-	Vec3 evaluate(float cos_theta) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in]  _ni :入射方向媒質の屈折率
+    * @param[in]  _no :出射方向媒質の屈折率
+    */
+    FresnelDielectric(float _ni, float _no);
+
+    Vec3 evaluate(float cos_theta) const override;
 
 private:
-	float ni, no;
+    float ni, no; /**< 屈折率 */
 };
 
-// *** 単層薄膜干渉 ***
-// float d  : 膜厚
-// float ni : 入射媒質の屈折率
-// float nf : 薄膜媒質の屈折率
-// float no : 出射媒質の屈折率
+
+/** 薄膜干渉フレネルクラス */
 class FresnelThinfilm : public Fresnel {
 public:
-	FresnelThinfilm(float d, float ni, float _nf, float _no);
-	Vec3 evaluate(float cos_theta) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in]  _d :薄膜の膜厚
+    * @param[in] _ni :入射方向媒質の屈折率
+    * @param[in] _nf :薄膜の屈折率
+    * @param[in] _no :出射方向媒質の屈折率
+    */
+    FresnelThinfilm(float _d, float _ni, float _nf, float _no);
+    Vec3 evaluate(float cos_theta) const override;
 
 private:
-		float d;    // 膜厚
-		float ni;   // 入射媒質の屈折率
-		float nf;	// 薄膜の屈折率
-		float no;   // ベース媒質の屈折率
+    float d;          /**< 膜厚   */
+    float ni, nf, no; /**< 屈折率 */
 };

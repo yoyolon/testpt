16bd6b75196e2b7e530f188ee398a71ea4bc22d5
yoyolon <yoyograph74@gmail.com>
2022-12-15 16:42:12 +0900

    [fix] コーディング規則

--- scr/Material.cpp
+++ scr/Material.cpp
@@ -9,13 +9,14 @@
 Diffuse::Diffuse(Vec3 _albedo) : Material(MaterialType::Diffuse), albedo(_albedo) {}
 
 float Diffuse::sample_pdf(const Vec3& wi, const Vec3& wo) const {
-	return std::max(std::abs(get_cos(wo)) * invpi, epsilon);
+    return std::max(std::abs(get_cos(wo)) * invpi, epsilon);
 }
+
 bool Diffuse::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const {
-	wo = Random::cosine_hemisphere_sample();
-	pdf = sample_pdf(wi, wo);
-	brdf = albedo * invpi;
-	return true;
+    wo = Random::cosine_hemisphere_sample();
+    pdf = sample_pdf(wi, wo);
+    brdf = albedo * invpi;
+    return true;
 }
 
 
@@ -23,62 +24,61 @@ bool Diffuse::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, flo
 Mirror::Mirror(Vec3 _albedo) : Material(MaterialType::Specular), albedo(_albedo) {}
 
 float Mirror::sample_pdf(const Vec3& wi, const Vec3& wo) const {
-	return 1.0f;
+    return 1.0f;
 }
+
 bool Mirror::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const {
-	wo = unit_vector(Vec3(-wi.get_x(), -wi.get_y(), wi.get_z())); // 正反射方向
-	pdf = sample_pdf(wi, wo);
-	float cos_term = dot(Vec3(0,0,1), unit_vector(wo));
-	brdf = albedo / cos_term;
-	return true;
+    wo = unit_vector(Vec3(-wi.get_x(), -wi.get_y(), wi.get_z())); // 正反射方向
+    pdf = sample_pdf(wi, wo);
+    float cos_term = dot(Vec3(0,0,1), unit_vector(wo));
+    brdf = albedo / cos_term;
+    return true;
 }
 
 
 // *** Phongモデル ***
 Phong::Phong(Vec3 _albedo, Vec3 _Kd, Vec3 _Ks, float _shin) 
-	: Material(MaterialType::Glossy), albedo(_albedo), Kd(_Kd), Ks(_Ks), shin(_shin) {}
+    : Material(MaterialType::Glossy), albedo(_albedo), Kd(_Kd), Ks(_Ks), shin(_shin) {}
 
 float Phong::sample_pdf(const Vec3& wi, const Vec3& wo) const {
-	return std::max(std::abs(get_cos(wo)) * invpi, epsilon);
+    return std::max(std::abs(get_cos(wo)) * invpi, epsilon);
 }
 
 bool Phong::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const {
-	// 余弦に従ったサンプリング
-	wo = Random::cosine_hemisphere_sample();
-	pdf = sample_pdf(wi, wo);
-	auto dir_spec = unit_vector(Vec3(-wi.get_x(), -wi.get_y(), wi.get_z())); // 正反射方向
-	Vec3 diffuse = Kd * invpi;
-	float cos_term = dot(Vec3(0, 0, 1), unit_vector(wo));
-	float sp = std::pow(std::max(0.0f, dot(dir_spec, wo)), shin);
-	Vec3 specular = Ks / cos_term  * sp * invpi;
-	brdf = albedo * (diffuse + specular);
-	return true;
+    wo = Random::cosine_hemisphere_sample();
+    pdf = sample_pdf(wi, wo);
+    auto dir_spec = unit_vector(Vec3(-wi.get_x(), -wi.get_y(), wi.get_z())); // 正反射方向
+    Vec3 diffuse = Kd * invpi;
+    float cos_term = dot(Vec3(0, 0, 1), unit_vector(wo));
+    float sp = std::pow(std::max(0.0f, dot(dir_spec, wo)), shin);
+    Vec3 specular = Ks / cos_term  * sp * invpi;
+    brdf = albedo * (diffuse + specular);
+    return true;
 }
 
 
 // *** マイクロファセットモデル ***
-// 参考: https://www.pbr-book.org/3ed-2018/Reflection_Models/Microfacet_Models
 Microfacet::Microfacet(Vec3 _albedo, std::shared_ptr<MicrofacetDistribution> _distribution, 
-					   std::shared_ptr<Fresnel> _fresnel)
-	: Material(MaterialType::Glossy), albedo(_albedo), distribution(_distribution), fresnel(_fresnel) {}
+                       std::shared_ptr<Fresnel> _fresnel)
+    : Material(MaterialType::Glossy), albedo(_albedo), distribution(_distribution), fresnel(_fresnel) {}
 
 float Microfacet::sample_pdf(const Vec3& wi, const Vec3& h) const {
-	return distribution->sample_pdf(wi, h) / (4 * dot(wi, h)); // 確率密度の変換
+    return distribution->sample_pdf(wi, h) / (4 * dot(wi, h)); // 確率密度の変換
 }
 
 bool Microfacet::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const {
-	Vec3 h = distribution->sample_halfvector();
-	wo = unit_vector(reflect(wi, h)); // ハーフベクトルと入射方向から出射方向を計算
-	pdf = sample_pdf(wi, h);
-	float cos_wi = std::abs(get_cos(wi));
-	float cos_wo = std::abs(get_cos(wo));
-	if (cos_wi == 0 || cos_wo == 0) return false;
-	if (h.get_x() == 0 && h.get_y() == 0 && h.get_z() == 0) return false;
-	float D = distribution->D(h);
-	float G = distribution->G(wi, wo);
-	Vec3 F = fresnel->evaluate(dot(wi, h));
-	brdf = albedo *  (D * G * F) / (4 * cos_wi * cos_wo);
-	return true;
+    Vec3 h = distribution->sample_halfvector();
+    wo = unit_vector(reflect(wi, h)); // ハーフベクトルと入射方向から出射方向を計算
+    pdf = sample_pdf(wi, h);
+    float cos_wi = std::abs(get_cos(wi));
+    float cos_wo = std::abs(get_cos(wo));
+    if (cos_wi == 0 || cos_wo == 0) return false;
+    if (h.get_x() == 0 && h.get_y() == 0 && h.get_z() == 0) return false;
+    float D = distribution->D(h);
+    float G = distribution->G(wi, wo);
+    Vec3 F = fresnel->evaluate(dot(wi, h));
+    brdf = albedo *  (D * G * F) / (4 * cos_wi * cos_wo);
+    return true;
 }
 
 
@@ -87,9 +87,9 @@ Emitter::Emitter(Vec3 _intensity) : Material(MaterialType::Emitter), intensity(_
 
 float Emitter::sample_pdf(const Vec3& wi, const Vec3& wo) const { return 1.0; }
 bool Emitter::f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const {
-	return false;
+    return false;
 }
 
 Vec3 Emitter::emitte() const {
-	return intensity;
+    return intensity;
 }
--- scr/Material.h
+++ scr/Material.h
@@ -1,104 +1,215 @@
+/**
+* @file  Material.h
+* @brief マテリアル
+* @note: シェーディング規則
+*        1.法線をz軸正の方向としたシェーディング座標系で行う
+*        2.入射/出射方向は物体表面から離れる方向を正とする
+*        3.z軸とベクトルがなす角をthetaとする．
+*/
 #pragma once
 
 #include "Ray.h"
 
-// *** シェーディング規則 ***
-// 1. 法線をz軸正の方向としたシェーディング座標系で行う
-// 2. 入射/出射方向は物体表面から離れる方向を正とする
-// 3. z軸とベクトルがなす角をthetaとする．
 
 struct intersection;
 
 enum class MaterialType {
-	Diffuse = 1, Specular = 2, Glossy = 4, Emitter = 8
+    Diffuse  = 1, /**< 拡散反射 */
+    Specular = 2, /**< 鏡面反射 */
+    Glossy   = 4, /**< 光沢反射 */
+    Emitter  = 8  /**< 発光     */
 };
 
-// *** ユーティリティ関数 ***
+/**
+* @brief 入射角余弦を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角余弦
+*/
 inline float get_cos(const Vec3& w) { return w.get_z(); }
+
+/**
+* @brief 入射角余弦の二乗を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角余弦の二乗
+*/
 inline float get_cos2(const Vec3& w) { return w.get_z() * w.get_z(); }
+
+/**
+* @brief 入射角正弦の二乗を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角正弦の二乗
+*/
 inline float get_sin2(const Vec3& w) { return std::max(0.0f, 1.0f - get_cos2(w)); }
+
+/**
+* @brief 入射角正弦を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角正弦
+*/
 inline float get_sin(const Vec3& w) { return std::sqrt(get_sin2(w)); }
+
+/**
+* @brief 入射角正接を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角正接
+*/
 inline float get_tan(const Vec3& w) { return get_sin(w) / get_cos(w); }
+
+/**
+* @brief 入射角正接の二乗を計算する関数
+* @param[in]  w :方向ベクトル
+* @return float :入射角正接の二乗
+*/
 inline float get_tan2(const Vec3& w) { return get_sin2(w) / get_cos2(w); }
 
-// *** マテリアルクラス ***
+
+/** マテリアルの抽象クラス */
 class Material {
 public:
-	virtual ~Material() {};
-	Material(MaterialType _type) : type(_type) {};
-	// f   : 入射方向に対して反射方向をサンプリングしてBRDFを評価する
-	// wi  : 入射方向
-	// p   : 交差点
-	// brdf: 評価するBRDF
-	// wo  : 反射方向
-	// pdf : 立体角に関するサンプリング確率密度
-	virtual bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const = 0;
-	virtual Vec3 emitte() const { return Vec3(0.0f, 0.0f, 0.0f); }
-	virtual float sample_pdf(const Vec3& wi, const Vec3& wo) const = 0;
+    virtual ~Material() {};
 
-	MaterialType type;
+    /**
+    * @brief コンストラクタ
+    * @param[in] _type :反射特性の種類
+    */
+    Material(MaterialType _type) : type(_type) {};
+
+    /**
+    * @brief 入射方向に対して反射方向をサンプリングしてBRDFを評価する関数
+    * @param[in]  wi   :入射方向ベクトル
+    * @param[in]  p    :物体表面の交差点情報
+    * @param[out] brdf :入射方向と出射方向に対するBRDFの値
+    * @param[out] wo   :出射方向ベクトル
+    * @param[out] pdf  :立体角に関するサンプリング確率密度
+    * @return bool     :反射材質ならTrue
+    * @note: 実際は入射方向をサンプリングするがBRDF相反性により反射方向のサンプリングとして実装した
+    */
+    virtual bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const = 0;
+
+    /**
+    * @brief 自己発光を評価する関数
+    * @return Vec3 : 発光による放射輝度
+    */
+    virtual Vec3 emitte() const { return Vec3(0.0f, 0.0f, 0.0f); }
+
+    /**
+    * @brief 出射方向のサンプリング確率密度を計算する関数
+    * @param[in] wi :入射方向ベクトル
+    * @param[in] wo :出射方向ベクトル
+    * @return float :出射方向のサンプリング確率密度
+    */
+    virtual float sample_pdf(const Vec3& wi, const Vec3& wo) const = 0;
+
+    /**
+    * @brief 材質の反射特性を取得する関数
+    * @return MaterialType :材質の反射特性
+    */
+    MaterialType get_type() const { return type; }
+
+private:
+    MaterialType type; /**> 反射特性 */
 };
 
 
-// *** 拡散反射 ***
+/** 拡散反射クラス */
 class Diffuse : public Material {
 public:
-	Diffuse(Vec3 _albedo);
-	bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
-	float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in] _albedo :反射係数
+    */
+    Diffuse(Vec3 _albedo);
+
+    bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
+
+    float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+
 private:
-	Vec3 albedo;
+    Vec3 albedo; /**> 反射係数 */
 };
 
 
-// *** 鏡面反射 ***
+/** 鏡面反射クラス */
 class Mirror : public Material {
 public:
-	Mirror(Vec3 _albedo);
-	bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
-	float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in] _albedo :反射係数
+    */
+    Mirror(Vec3 _albedo);
+
+    bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
+
+    float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+
 private:
-	Vec3 albedo;
+    Vec3 albedo; /**> 反射係数 */
 };
 
 
-// *** Phongモデル ***
+// *** Phong反射モデルクラス ***
 class Phong : public Material {
 public:
-	Phong(Vec3 _albedo, Vec3 Kd, Vec3 Ks, float shin);
-	bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
-	float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in] _albedo :反射係数
+    * @param[in] _Kd     :拡散反射係数
+    * @param[in] _Ks     :鏡面反射係数
+    * @param[in] _shin   :光沢度
+    */
+    Phong(Vec3 _albedo, Vec3 _Kd, Vec3 _Ks, float _shin);
+
+    bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
+
+    float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+
 private:
-	Vec3 albedo;
-	Vec3 Kd; // 拡散反射係数
-	Vec3 Ks; // 鏡面反射係数
-	float shin; // 光沢度
+    Vec3 albedo; /**> 反射係数     */
+    Vec3 Kd;     /**> 拡散反射係数 */
+    Vec3 Ks;     /**> 鏡面反射係数 */
+    float shin;  /**> 光沢度       */
 };
 
 
-// *** 発光 ***
-class Emitter : public Material {
-public:
-	Emitter(Vec3 _intensity);
-	bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
-	float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
-	Vec3 emitte() const;
-private:
-	Vec3 intensity; // 強度
-};
-
-
-// *** マイクロファセットBRDF ***
-// 参考: https://www.pbr-book.org/3ed-2018/Reflection_Models/Microfacet_Models
+/** マイクロファセット反射モデルクラス */
+/** @note 参考: https://www.pbr-book.org/3ed-2018/Reflection_Models/Microfacet_Models */
 class Microfacet : public Material {
 public:
-	Microfacet(Vec3 _albedo, std::shared_ptr<class MicrofacetDistribution> _distribution, 
-		       std::shared_ptr<class Fresnel> _fresnel);
-	bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
-	float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+    /**
+    * @brief コンストラクタ
+    * @param[in] _albedo       :反射係数
+    * @param[in] _distribution :マイクロファセット分布
+    * @param[in] _fresnel      :フレネルの式
+    */
+    Microfacet(Vec3 _albedo, std::shared_ptr<class MicrofacetDistribution> _distribution, 
+               std::shared_ptr<class Fresnel> _fresnel);
+
+    bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
+
+    float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
 
 private:
-	Vec3 albedo;
-	std::shared_ptr<class MicrofacetDistribution> distribution; // マイクロファセット分布
-	std::shared_ptr<class Fresnel> fresnel; // フレネル項
+    Vec3 albedo; /**> 反射係数     */
+    std::shared_ptr<class Fresnel> fresnel; /**> フレネル項 */
+    std::shared_ptr<class MicrofacetDistribution> distribution; /**> マイクロファセット分布 */
+};
+
+
+/** 発光クラス */
+class Emitter : public Material {
+public:
+    /**
+    * @brief コンストラクタ
+    * @param[in] _intensity :放射輝度
+    */
+    Emitter(Vec3 _intensity);
+
+    bool f(const Vec3& wi, const intersection& p, Vec3& brdf, Vec3& wo, float& pdf) const override;
+
+    float sample_pdf(const Vec3& wi, const Vec3& wo) const override;
+
+    Vec3 emitte() const;
+
+private:
+    Vec3 intensity; /**> 放射輝度 */
 };

0b9ac777511680d6e8b296d59702189d21b1e42e
yoyolon <yoyograph74@gmail.com>
2022-12-20 18:15:01 +0900

    [fix] 自己交差回避用の定数を修正

--- scr/utility.h
+++ scr/utility.h
@@ -11,7 +11,7 @@ constexpr float inf = std::numeric_limits<float>::infinity();
 constexpr float pi = 3.14159265359f;
 constexpr float invpi = 1.0f / pi;
 constexpr float epsilon = std::numeric_limits<float>::epsilon();
-constexpr float eps_isect = 0.0001f;
+constexpr float eps_isect = 0.01f;
 // *** ä÷êî ***
 inline float to_degree(float radian) { return 180.0f * radian / pi; }
 inline float to_radian(float degree) { return pi * degree / 180.0f; }

c03232ed49e0d4967b631e3d91d7c2376237fae0
yoyolon <yoyograph74@gmail.com>
2022-12-22 17:28:37 +0900

    [fix] 入射方向wを正規化

--- scr/Shape.cpp
+++ scr/Shape.cpp
@@ -1,8 +1,8 @@
 #include "Shape.h"
 
 float Shape::sample_pdf(const intersection& ref, const Vec3& w) const {
-    auto r = Ray(ref.pos, w); // refからジオメトリへのレイ
-    intersection isect;       // ジオメトリの交差点
+    auto r = Ray(ref.pos, unit_vector(w)); // refからジオメトリへのレイ
+    intersection isect;                    // ジオメトリの交差点
     if (!intersect(r, eps_isect, inf, isect)) {
         return 0.0f;
     }

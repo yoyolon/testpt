051f63ae258b163f6df5460ba9c8aea1d066d84d
yoyolon <yoyograph74@gmail.com>
2023-01-06 18:41:09 +0900

    [add] 交差点情報に表裏情報を追加

--- scr/Shape.h
+++ scr/Shape.h
@@ -18,7 +18,8 @@ enum class IsectType {
 struct intersection {
     Vec3 pos;                            /**< 座標             */
     Vec3 normal;                         /**< 法線             */
-    float t;                             /**< レイのパラメータ */
+    float t=0.0f;                        /**< レイのパラメータ */
+    bool is_front=true;                  /**< 交差点の裏表     */
     IsectType type;                      /**< 交差点の種類     */
     std::shared_ptr<class Material> mat; /**< 材質の種類       */
     std::shared_ptr<class Light> light;  /**< 光源の種類       */

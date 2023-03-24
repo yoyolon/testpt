dbc67201945139cbf43b5dc5aa5a44b0a495059a
yoyolon <yoyograph74@gmail.com>
2023-03-25 00:03:01 +0900

    [add] 環境マップの実装(仮)

--- scr/Light.cpp
+++ scr/Light.cpp
@@ -1,3 +1,9 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define __STDC_LIB_EXT1__
+
+#include "external/stb_image_write.h"
+#include "external/stb_image.h"
 #include "Light.h"
 #include "Shape.h"
 #include "Random.h"
@@ -18,7 +24,7 @@ AreaLight::AreaLight(Vec3 _intensity, std::shared_ptr<class Shape> _shape)
     area = shape->area();
 }
 
-Vec3 AreaLight::emitte() const {
+Vec3 AreaLight::evel_light(const Vec3& w) const {
     return intensity;
 }
 
@@ -26,11 +32,11 @@ Vec3 AreaLight::power() const {
     return intensity * area * 4 * pi;
 }
 
-Vec3 AreaLight::sample_light(const intersection& ref, Vec3& w, float& pdf) {
+Vec3 AreaLight::sample_light(const intersection& ref, Vec3& w, float& pdf) const {
     auto isect = shape->sample(ref);
     w = unit_vector(isect.pos - ref.pos);
     pdf = shape->eval_pdf(ref, w);
-    return emitte();
+    return evel_light(w);
 }
 
 float AreaLight::eval_pdf(const intersection& ref, const Vec3& w) const {
@@ -39,4 +45,82 @@ float AreaLight::eval_pdf(const intersection& ref, const Vec3& w) const {
 
 bool AreaLight::intersect(const Ray& r, float t_min, float t_max, intersection& p) const {
     return shape->intersect(r, t_min, t_max, p);
+}
+
+
+// *** 環境光源(IBL) ***
+EnvironmentLight::EnvironmentLight(std::string filename) 
+    : Light(LightType::IBL), nw(1), nh(1), nc(3), brightness(0.f)
+{
+    envmap = stbi_loadf("asset/envmap.hdr", &nw, &nh, &nc, 0);
+    if (envmap != nullptr) {
+        // 輝度マップを生成した
+        auto brightmap = std::make_unique<float[]>(nw * nh);
+        for (int h = 0; h < nh; h++) {
+            float sin_theta = std::sin(pi * float(h + 0.5f) / nh);
+            for (int w = 0; w < nw; w++) {
+                int index = h * nw * 3 + w * 3;
+                // sRGBからXYZのY成分を計算
+                float R = envmap[index++];
+                float G = envmap[index++];
+                float B = envmap[index];
+                float brightness_element = 0.2126f * R + 0.7152f * G + 0.0722f * B;
+                brightness += brightness_element;
+                brightmap[h * nw + w] = brightness_element * sin_theta;
+            }
+        }
+        brightness /= (nw * nh);
+        dist = std::make_unique<Piecewise2D>(brightmap.get(), nw, nh);
+    }
+}
+
+Vec3 EnvironmentLight::evel_light(const Vec3& w) const {
+    if (envmap == nullptr) {
+        return Vec3(0.0f, 0.0f, 0.0f);
+    }
+    // 方向からuv座標を計算
+    Vec3 dir = unit_vector(w);
+    float u = std::atan2(dir.get_z(), dir.get_x()) + pi;
+    u *= invpi * 0.5;
+    float v = std::acos(std::clamp(dir.get_y(), -1.0f, 1.0f)) * invpi;
+    // 環境マップから放射輝度をサンプリング
+    int x = std::clamp((int)(nw * u), 0, nw - 1);
+    int y = std::clamp((int)(nh * v), 0, nh - 1);
+    int index = y * nw * 3 + x * 3;
+    float R = envmap[index++];
+    float G = envmap[index++];
+    float B = envmap[index];
+    return Vec3(R, G, B);
+}
+
+Vec3 EnvironmentLight::power() const {
+    float radius = 100;
+    return pi * radius * radius * brightness;
+}
+
+Vec3 EnvironmentLight::sample_light(const intersection& ref, Vec3& wo, float& pdf) const {
+    Vec2 uv = dist->sample(pdf);
+    float phi = 2 * pi * uv[0];
+    float theta = uv[1] * pi;
+    wo = Vec3(std::sin(theta) * std::cos(phi), std::sin(theta) * std::sin(phi), std::cos(theta));
+    pdf /= 2 * pi * pi * std::sin(theta);
+    return evel_light(wo);
+}
+
+float EnvironmentLight::eval_pdf(const intersection& ref, const Vec3& w) const {
+    float theta = std::acos(w.get_z());
+    float phi = std::atan2(w.get_y(), w.get_x());
+    if (phi < 0) phi = phi + 2 * pi;
+    float sin_theta = std::sin(theta);
+    if (sin_theta == 0) return 0;
+    return dist->eval_pdf(Vec2(phi * invpi, theta * invpi)) / (2 * pi * pi * sin_theta);
+}
+
+bool EnvironmentLight::intersect(const Ray& r, float t_min, float t_max, intersection& p) const {
+    // 交差点情報の更新
+    p.t = inf;
+    p.pos = Vec3::zero;
+    p.normal = Vec3::zero;
+    p.is_front = true;
+    return true;
 }
--- scr/Light.h
+++ scr/Light.h
@@ -9,6 +9,7 @@
 #include "Ray.h"
 
 struct intersection;
+class Piecewise2D;
 class Scene;
 class Shape;
 
@@ -31,9 +32,10 @@ public:
 
     /**
     * @brief 光源の放射輝度を計算する関数
+    * @param[in] w :放射輝度が沿う方向
     * @return Vec3 :光源の放射輝度
     */
-    virtual Vec3 emitte() const = 0;
+    virtual Vec3 evel_light(const Vec3& w) const = 0;
 
     /**
     * @brief 光源の放射エネルギーを計算する関数
@@ -49,7 +51,7 @@ public:
     * @return Vec3    :光源からの入射方向
     * @note: 実際は光源からの入射方向は反転する(視線方向から追跡するため)
     */
-    virtual Vec3 sample_light(const intersection& ref, Vec3& w, float& pdf) = 0;
+    virtual Vec3 sample_light(const intersection& ref, Vec3& w, float& pdf) const = 0;
 
     /**
     * @brief 入射方向から光源サンプリングの確率密度を返す関数
@@ -94,12 +96,12 @@ public:
     */
     AreaLight(Vec3 _intensity, std::shared_ptr<Shape> _shape);
 
-    Vec3 emitte() const override;
+    Vec3 evel_light(const Vec3& w) const override;
 
     Vec3 power() const override;
 
 
-    Vec3 sample_light(const intersection& ref, Vec3& wo, float& pdf) override;
+    Vec3 sample_light(const intersection& ref, Vec3& wo, float& pdf) const override;
 
     float eval_pdf(const intersection& ref, const Vec3& w) const override;
 
@@ -112,27 +114,31 @@ private:
 };
 
 
-//// *** 環境光源(IBL) ***
-//class EnvironmentLight : public Light {
-//public:
-//    /**
-//    * @brief 面光源の初期化
-//    * @param[in] _intensity :光源の放射輝度
-//    * @param[in] _shape     :光源のジオメトリ
-//    */
-//    EnvironmentLight(std:: string filename);
-//
-//    Vec3 emitte() const override;
-//
-//    Vec3 power() const override;
-//
-//
-//    Vec3 sample_light(const intersection& ref, Vec3& wo, float& pdf) override;
-//
-//    float eval_pdf(const intersection& ref, const Vec3& w) const override;
-//
-//    bool intersect(const Ray& r, float t_min, float t_max, intersection& p) const override;
-//
-//private:
-//    //float* envmap; /**< 環境マップ */
-//};
+// *** 環境光源(IBL) ***
+class EnvironmentLight : public Light {
+public:
+    /**
+    * @brief 面光源の初期化
+    * @param[in] _intensity :光源の放射輝度
+    * @param[in] _shape     :光源のジオメトリ
+    */
+    EnvironmentLight(std:: string filename);
+
+    Vec3 evel_light(const Vec3& w) const override;
+
+    Vec3 power() const override;
+
+    Vec3 sample_light(const intersection& ref, Vec3& wo, float& pdf) const override;
+
+    float eval_pdf(const intersection& ref, const Vec3& w) const override;
+
+    bool intersect(const Ray& r, float t_min, float t_max, intersection& p) const override;
+
+private:
+    float* envmap;    /**< 環境マップ   */
+    int nw;           /**< 幅           */
+    int nh;           /**< 高さ         */
+    int nc;           /**< チャンネル数 */
+    float brightness; /**< 明るさ       */
+    std::unique_ptr<Piecewise2D> dist;  /**< 環境マップの輝度分布 */
+};

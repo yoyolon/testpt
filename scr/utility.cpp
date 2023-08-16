29825d84f75c9c6dc37accacfbc967c061adfa5d
yoyolon <yoyograph74@gmail.com>
2023-08-16 19:39:38 +0900

    [fix] 逆ガンマ補正と文字列分離の追加

--- scr/utility.cpp
+++ scr/utility.cpp
@@ -1,30 +1,61 @@
 #include "utility.h"
+#include <sstream>
 #include "Math.h"
 
 
 float gamma_correction_element(float c) {
+    //return std::powf(c, 1 / 2.4f);
     // ガンマ補正
-    if (c < 0.0031308f) {
+    if (c <= 0.0031308f) {
         return 12.92f * c;
     }
     return 1.055f * std::powf(c, 1 / 2.4f) - 0.055f;
 }
 
 Vec3 gamma_correction(const Vec3& color) {
-    // ガンマ補正
     auto r = gamma_correction_element(color.get_x());
     auto g = gamma_correction_element(color.get_y());
     auto b = gamma_correction_element(color.get_z());
     return Vec3(r, g, b);
 }
 
+float inv_gamma_correction_element(float c) {
+    //return std::powf(c, 2.4f);
+    // 逆ガンマ補正
+    if (c <= 0.04045f) {
+        return  c / 12.92f;
+    }
+    return (c + 0.055f) / (std::powf(1.055f, 2.4f));
+}
+
+Vec3 inv_gamma_correction(const Vec3& color) {
+    auto r = inv_gamma_correction_element(color.get_x());
+    auto g = inv_gamma_correction_element(color.get_y());
+    auto b = inv_gamma_correction_element(color.get_z());
+    return Vec3(r, g, b);
+}
+
+
 Vec3 exclude_invalid(const Vec3& color) {
     float r = color.get_x();
     float g = color.get_y();
     float b = color.get_z();
     // 無効な値をゼロにする
-    if (!isfinite(r)) r = 0.0f;
-    if (!isfinite(g)) g = 0.0f;
-    if (!isfinite(b)) b = 0.0f;
+    if (!isfinite(r)) r = 0.f;
+    if (!isfinite(g)) g = 0.f;
+    if (!isfinite(b)) b = 0.f;
     return Vec3(r, g, b);
+}
+
+
+std::vector<std::string> split_string(const std::string& line, char delimiter) {
+    std::stringstream ss(line);
+    std::string temp;
+    std::vector<std::string> ret;
+    // EOFまで入力ストリームをdelimiterで区切る
+    while (std::getline(ss, temp, delimiter)) {
+        if (temp == "") continue; // 空行はスキップ
+        ret.push_back(temp);
+    }
+    return ret;
 }
--- scr/utility.h
+++ scr/utility.h
@@ -1,9 +1,15 @@
+/**
+* @file  utility.h
+* @brief 便利な定数や関数などを実装
+*/
+
 #pragma once
 
 #include <cmath>
 #include <limits>
 #include <memory>
 #include <random>
+#include <string>
 #include <vector>
 
 class Vec3;
@@ -16,6 +22,9 @@ constexpr float inf = std::numeric_limits<float>::infinity();
 // 円周率
 constexpr float pi = 3.14159265359f;
 
+// 円周率の半分
+constexpr float pi_half = pi / 2;
+
 // 円周率の逆数
 constexpr float invpi = 1.0f / pi;
 
@@ -26,21 +35,39 @@ constexpr float epsilon = std::numeric_limits<float>::epsilon();
 constexpr float eps_isect = 0.01f;
 
 
-
 // *** 関数 ***
+
 /**
-* @brief ガンマ補正のヘルパー関数
+* @brief sRGBのガンマ補正のヘルパー関数
 * @param[in]  c  :ガンマ補正前の色の要素
 * @return Vfloat :ガンマ補正後の色の要素
 */
 float gamma_correction_element(float c);
 
+
 /**
-* @brief ガンマ補正
+* @brief sRGBのガンマ補正を行う関数
 * @param[in]  color :ガンマ補正前の色
 * @return Vec3      :ガンマ補正後の色
 */
 Vec3 gamma_correction(const Vec3& color);
+
+
+/**
+* @brief sRGBの逆ガンマ補正のヘルパー関数
+* @param[in]  c  :逆ガンマ補正前の色の要素
+* @return Vfloat :逆ガンマ補正後の色の要素
+*/
+float inv_gamma_correction_element(float c);
+
+
+/**
+* @brief sRGBの逆ガンマ補正を行う関数
+* @param[in]  color :逆ガンマ補正前の色
+* @return Vec3      :逆ガンマ補正後の色
+*/
+Vec3 inv_gamma_correction(const Vec3& color);
+
 
 /**
 * @brief 輝度から無効な値(NaNやinf)を除外する関数
@@ -48,3 +75,12 @@ Vec3 gamma_correction(const Vec3& color);
 * @return Vec3      :有効値に変換後の値
 */
 Vec3 exclude_invalid(const Vec3& color);
+
+
+/**
+* @brief 文字列を指定した文字で分割する関数
+* @param[in]  line      :分割する文字列
+* @param[in]  delimiter :区切り文字
+* @return std::vector<std::string> 分割後の文字列の配列
+*/
+std::vector<std::string> split_string(const std::string& line, char delimiter = ' ');
